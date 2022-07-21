30fa7821224ae470461005ffb12365bb2bf6b7b3
jtran <jtran4481@gmail.com>
2022-07-21 13:02:38 -0600

    only database left to integrate

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from typing import Optional
-from fastapi import FastAPI
+from typing import List, Optional
+from urllib import response
+from fastapi import FastAPI, Query
+from fastapi.encoders import jsonable_encoder
 from model.dbHandler import match_exact, match_like
 
-app = Flask(__name__)
+app = FastAPI()
 
 
 @app.get("/")
@@ -12,11 +14,12 @@ def index():
     This method will
     1. Provide usage instructions formatted as JSON
     """
-    return "TODO"
+    response = {'usage':'/dict?=<word>'}
+    return jsonable_encoder(response)
 
 
 @app.get("/dict")
-def dictionary():
+def dictionary(words: List[str] = Query(None)):
     """
     DEFAULT ROUTE
     This method will
@@ -24,4 +27,26 @@ def dictionary():
     2. Try to find an exact match, and return it if found
     3. If not found, find all approximate matches and return
     """
-    return "TODO"
+
+    if not words:
+        response = {'status':'error', 'word':words, 'data':'word not found'}
+        return jsonable_encoder(response)
+    
+    response = {'words':[]}
+
+    for word in words:
+        definitions = match_exact(word)
+        if definitions:
+            response['words'].append({'status':'success', 'word':word, 'data':definitions})
+        else:
+
+            definitions = match_like(word)
+            if definitions:
+                response['words'].append({'status':'partial', 'word':word, 'data':definitions})
+                
+            else:
+                response['words'].append({'status':'error', 'word':word, 'data':'word not found'})
+            
+    return jsonable_encoder(response)
+
+
